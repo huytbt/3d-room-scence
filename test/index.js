ac3e4bf6550a4b6e177bf0528a42bfe3c9e93f35
huytbt <huytbt@gmail.com>
2016-10-06 17:18:51 +0700

    feat(Package): Initial
    Test Coverage
    
    see #1

--- test/index.js
+++ test/index.js
@@ -1,5 +1,12 @@
-// var Chicken = require('../src');
+let chai = require('chai');
+let Chicken = require('../src');
+
+chai.should();
 
 describe('Hello', () => {
-  it('test');
+  it('test', () => {
+    let a = new Chicken.Room();
+    a.cameraPosition.x = 10;
+    a.cameraPosition.x.should.equal(10);
+  });
 });
